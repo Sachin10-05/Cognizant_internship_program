3c3227aed9e2199d519ff7da94bb2198fdea2f93
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-04-28 11:56:23 +0530

    Create DML - Insert Records - Tickets.sql

---
+++ oracle SQL/DML - Insert Records - Tickets.sql
@@ -0,0 +1,7 @@
+/*dml2.sql*/
+
+insert into TICKETS (Ticket_id,Schedule_id,User_id,No_seats)
+values('T1','S5',1,2);
+
+insert into TICKETS (Ticket_id,Schedule_id,User_id,No_seats)
+values('T2','S2',5,1);
