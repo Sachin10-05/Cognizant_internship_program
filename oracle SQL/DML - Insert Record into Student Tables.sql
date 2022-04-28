5578e69017a8153bccbc51f7f6d85568c533eb50
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-04-28 11:57:37 +0530

    Create DML - Insert Record into Student Tables.sql

---
+++ oracle SQL/DML - Insert Record into Student Tables.sql
@@ -0,0 +1,16 @@
+/*sample.sql*/
+insert into STUDENT(Studid,FirstName,LastName,Street,City,DOB)
+values(3001,'Dileep','Kumar','JaiNagar','Bangalore','10-March-1999');
+
+insert into STUDENT(Studid,FirstName,LastName,Street,City,DOB)
+values(3002,'Anand','Kumar','Indiranagar','Bangalore','19-January-1998');
+
+insert into STUDENT(Studid,FirstName,LastName,Street,City,DOB)
+values(3003,'Bala','Krishnan','Annanagar','Chennai','03-January-1996');
+
+insert into STUDENT(Studid,FirstName,LastName,Street,City,DOB)
+values(3004,'Gowri','Shankar','Gandhipuram','Coimbatore','22-December-1997');
+
+insert into STUDENT(Studid,FirstName,LastName,Street,City,DOB)
+values(3005,'Priya','Menon','JPNagar','Cochin','12-February-1994');
+
