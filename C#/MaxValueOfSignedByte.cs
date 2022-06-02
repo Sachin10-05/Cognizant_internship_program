7b77df1c0a86b18e7eef84f52b794326e10b2e33
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-06-02 15:03:41 +0530

    Create MaxValueOfSignedByte.cs

---
+++ C#/MaxValueOfSignedByte.cs
@@ -0,0 +1,15 @@
+//************************************************************************Program.cs*********************************************************************************
+
+using System;
+
+public class Program      //DO NOT change the class name
+{
+    //implement code here
+    static void Main(string[] args)
+    {
+        sbyte number = 125;
+        Console.WriteLine("Value of number: " + number);
+        Console.WriteLine("Largest value stored in a signed byte: " + "127");
+    }
+    
+}
