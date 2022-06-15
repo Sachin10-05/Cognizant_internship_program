4dc043f3b4313bb8d09df83c0ae77ffd16ba84fd
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-06-15 21:30:21 +0530

    Create Hello world.cs

---
+++ C#/Hello world.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
