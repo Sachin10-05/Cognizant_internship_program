ff21dd2ffd64050af479499ec35d8f8b474221f2
Sachin Kumar <104192958+Sachin10-05@users.noreply.github.com>
2022-06-15 21:27:30 +0530

    Create Get input.cs

---
+++ C#/Get input.cs
@@ -0,0 +1,18 @@
+using System;
+namespace program
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name");
+            string fname = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            String lname = Console.ReadLine();
+            
+            Console.WriteLine($"Your full name is {fname} {lname}");
+            Console.ReadLine();
+            
+        }
+    }
+}
